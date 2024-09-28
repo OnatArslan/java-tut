1b49c05b63124cb47c374f9e68a724ef217b9a95
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-28 06:06:46 +0300

    Commit for changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("First java code this year");
     }
+
+
 
 }
