a3c40e76bb14f5ecb6132f21752b414bbe3a77ba
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-28 06:20:45 +0300

    Commit for changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("First java code this year");
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+
+
+
     }
 
 
