5c831d41f47b6afa4131e5ce0523f3aa26bb04e8
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-29 07:08:13 +0300

    Working with dates

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,13 @@
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Onat");
+
+        Date curDate = new Date(2004,11,2);
+        System.out.println(curDate.toString());
+
+
 
 
     }
