e69e3dd6b7656be553f79f9ae342acd1cdd34b1e
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-29 08:29:03 +0300

    Commit for changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,14 @@
+import java.util.Calendar;
 import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Onat");
 
-        Date curDate = new Date(2004,11,2);
+        Date curDate = new Date();
         System.out.println(curDate.toString());
 
-
+        int[] numbers = {1, 2, 3, 4, 5};
 
 
     }
