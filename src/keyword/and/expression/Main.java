75f0c00442c053a20a283418174e12f68630ead7
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-29 09:59:10 +0300

    Commit for changes

--- src/keyword/and/expression/Main.java
+++ src/keyword/and/expression/Main.java
@@ -4,5 +4,26 @@ package keyword.and.expression;
 public class Main {
     public static void main(String[] args) {
 
+        int highScore = 50;
+
+        int lowScore = 10;
+        if (highScore > 25) {
+            highScore += 1000;
+        }
+
+        System.out.println(highScore);
+
+
+
+
+
+
+
+
+
+
+
+        int a = 12;
+
     }
 }
