1e47ce99d2643cc4f74ac246400612b5e6558a2a
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-03 00:31:12 +0300

    Commit for changes

--- src/keyword/and/expression/Main.java
+++ src/keyword/and/expression/Main.java
@@ -1,28 +1,40 @@
 package keyword.and.expression;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        int highScore = 50;
+        String name = "Onat Arslan";
+        String upperName = name.toUpperCase();
+        int indexOfA = name.indexOf("A");
+        System.out.println("upperName = " + upperName);
+        System.out.println("indexOfA = " + indexOfA);
 
-        int lowScore = 10;
-        if (highScore > 25){
-
-            highScore += 1000;}
-
-        System.out.println(highScore);
-        int a = 12;
+        List<String> brands = new ArrayList<>();
+        brands.add("Volvo");
+        brands.add("Mercedes");
+        brands.add("Bmw");
+        brands.add("Opel");
+        for (String brand : brands) {
+            System.out.println(brand.startsWith("V"));
         }
 
+        for (int index = 0; index < brands.size(); index++) {
+            System.out.println("Element "+brands.get(index)+" at index: "+ index);
+        }
+
+        String[] names = {"Onat","Rugi","Coni"};
 
 
-
-
-
-
-
-        int numA = 12;
-
+        for (String n : names) {
+            System.out.println(n);
+        }
     }
+
+
+}
 
