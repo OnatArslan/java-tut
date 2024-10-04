29008c9990a1a850d6ead57da45116244bd37195
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-04 05:54:43 +0300

    Commit for compare POJO and Record

--- src/pojo/Main.java
+++ src/pojo/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
             /* id will be unique because of + 1 statement */
-            RStudent s = new RStudent("S92300" + i, switch (i){
+            RStudent s = new RStudent("S92300" + i, switch (i) {
                 case 1 -> "Mary";
                 case 2 -> "Carol";
                 case 3 -> "Tim";
@@ -14,6 +14,12 @@ public class Main {
                 default -> "Anonymous";
             }, "05/11/1985", "Java Masterclass");
             System.out.println(s);
+
         }
+        PojoStudent pojoStudent = new PojoStudent("100001", "Onat", "29/11/2001", "Python");
+        RStudent recordStudent = new RStudent("100002", "Rugi", "01/01/2004", "Economy");
+
+        System.out.println(pojoStudent.getName());
+        System.out.println(recordStudent.name());
     }
 }
