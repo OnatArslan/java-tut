c56d84f61b907ed65bc15c04606ac79034c42ff1
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 23:52:52 +0300

    Commit for changes

---
+++ src/composition/PersonalComputer.java
@@ -0,0 +1,19 @@
+package composition;
+
+public class PersonalComputer extends Product {
+
+    private Monitor monitor;
+    private Motherboard motherboard;
+    private ComputerCase computerCase;
+
+    public PersonalComputer(String model, String manufacturer, Monitor monitor, Motherboard motherboard, ComputerCase computerCase) {
+        super(model, manufacturer);
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+        this.computerCase = computerCase;
+    }
+
+    public PersonalComputer(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+}
