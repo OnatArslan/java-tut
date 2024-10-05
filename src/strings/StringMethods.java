fc8974185cd4988a7430d6826071333c6794ffd4
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 07:20:05 +0300

    Commit for changes

---
+++ src/strings/StringMethods.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String birthDate = "25/11/1982";
+
+        /* Start index of given string or char */
+        System.out.println(birthDate.indexOf("1982"));
+
+        /* Like slice method 10. index not included */
+        System.out.println(birthDate.substring(6,10));
+
+
+    }
+}
