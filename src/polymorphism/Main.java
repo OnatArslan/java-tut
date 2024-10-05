919dab647acfd026a3478dbe14f1f3e06a466490
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 01:59:19 +0300

    Commit for changes

--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -8,15 +8,14 @@ public class Main {
 
         Movie movie = new Movie("Game of Thrones");
         Movie adventureMovie = new Adventure("Star Wars");
-        Movie comedyMovie = new Comedy("RecepVedik");
+        Movie comedyMovie = new Comedy("RecepVedic");
         /* Method name is the same, but all of them doing different thing this is polymorphism */
         movie.watchMovie();
         adventureMovie.watchMovie();
         comedyMovie.watchMovie();
-
-
-
     }
+
+
 }
 
 
