7e715bb878faf9844ef9c7422d8e5750d0c89134
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 04:40:38 +0300

    Commit for working inheritance

--- src/inheritance/Animal.java
+++ src/inheritance/Animal.java
@@ -1,4 +1,22 @@
 package inheritance;
 
 public class Animal {
+    private String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -3,5 +3,7 @@ package inheritance;
 public class Main {
     public static void main(String[] args) {
 
+        Animal firstAnimal = new Animal("duck", "small", 10.2);
+        System.out.println(firstAnimal);
     }
 }
