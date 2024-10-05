5d9bf598ac8fc953cd1fd1e68b822188c93bc8ea
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 05:06:24 +0300

    Commit for changes

--- src/inheritance/Dog.java
+++ src/inheritance/Dog.java
@@ -4,9 +4,9 @@ public class Dog extends Animal{
     private String earShape;
     private String tailShape;
 
-    public Dog(String type, String size, double weight, String earShape, String tailShape) {
+    public Dog(String type, double weight, String earShape, String tailShape) {
         /* Super() constructor is parent class constructor !!! */
-        super(type, size, weight);
+        super(type, weight < 15 ? "small" : (weight < 30 ? "medium" : "large"), weight);
         /* Field at belong are special fields for Dog(child) class not Animal(parent) class */
         this.earShape = earShape;
         this.tailShape = tailShape;
--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         /* Create parent class */
         Animal animal = new Animal("duck", "small", 10.2);
-        Dog dog = new Dog("pitbull", "medium", 22.90, "sharp", "short");
-
-        doAnimalStuff(dog, "slow");
+        Dog dog = new Dog("pitbull", 12.90, "sharp", "short");
+        doAnimalStuff(animal, "slow");
+        System.out.println("-------------------------");
+        doAnimalStuff(dog, "fast");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
