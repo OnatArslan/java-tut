0ddb31fe70dd537f7d094df3e04bca700d45664b
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 02:17:58 +0300

    Commit for changes

--- src/polymorphism/NextMain.java
+++ src/polymorphism/NextMain.java
@@ -1,6 +1,13 @@
 package polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NextMain {
     public static void main(String[] args) {
+        List<Integer> myListOfNumbers = new ArrayList<>();
+        myListOfNumbers.add(19);
+        myListOfNumbers.add(120);
+        System.out.println(myListOfNumbers);
     }
 }
