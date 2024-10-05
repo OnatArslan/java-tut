e5cb4f2c715c412cef46835e316f459f6cd1345a
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 05:57:14 +0300

    Commit for changes

---
+++ src/obj/Main.java
@@ -0,0 +1,11 @@
+package obj;
+
+import java.util.ArrayList;
+
+/* All java class inherit from Object class */
+public class Main extends Object{
+    public static void main(String[] args) {
+
+
+    }
+}
