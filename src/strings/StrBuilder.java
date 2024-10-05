1682ad32fe79ce2f6da1a807d41be6c10cb26e4f
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-05 08:02:18 +0300

    Commit for changes

--- src/strings/StrBuilder.java
+++ src/strings/StrBuilder.java
@@ -1,5 +1,8 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class StrBuilder {
     public static void main(String[] args) {
         String helloWorld = "Hello" + "World";
@@ -9,5 +12,28 @@ public class StrBuilder {
         helloWorldBuilder.append("and Goodbye");
         System.out.println(helloWorld); // Hello World
         System.out.println(helloWorldBuilder); // Hello World and Goodbye
+
+        /* String methods return new String */
+        /*  builder methods return a String builder but this is a self-reference*/
+
+        /* Special string builder methods */
+        StringBuilder newString = new StringBuilder("We must reverse this string");
+        /* reverse() */
+        newString.reverse();
+        System.out.println(newString);
+
+        /* insert() add string to given index(offset)*/
+        newString.insert(3, "hello");
+        System.out.println(newString);
+
+        /* delete() or deleteCharAt() */
+        StringBuilder secStr = new StringBuilder("Onat Arslan");
+        secStr.delete(6, 8);
+        secStr.deleteCharAt(0);
+        System.out.println(secStr);
+
+        secStr.replace(1, 5, "G");
+        System.out.println(secStr);
+
     }
 }
