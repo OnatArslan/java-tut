a0ee2aa7ea84090b2e9293c26584ce09ddcbf9c3
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 01:06:58 +0300

    Commit for changes

---
+++ src/encapsulation/Printer.java
@@ -0,0 +1,48 @@
+package encapsulation;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        if (tonerLevel <= 0) {
+            this.tonerLevel = 1;
+        } else if (tonerLevel > 100) {
+            this.tonerLevel = 100;
+        } else {
+            this.tonerLevel = tonerLevel;
+        }
+        this.pagesPrinted = pagesPrinted;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int amount) {
+        if (amount < 0) {
+            throw new RuntimeException("Amount of toner can not be negative number");
+        }
+        this.tonerLevel += amount;
+        if (tonerLevel > 100) {
+            System.out.println("Toner is full");
+            this.tonerLevel = 100;
+            return tonerLevel;
+        }
+        return tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        if (this.duplex) {
+            if (pages % 2 == 0) {
+                System.out.println("This is a duplex printer");
+                return pages / 2;
+            } else {
+                System.out.println("This is a duplex printer");
+                return pages / 2 + 1;
+            }
+
+        } else {
+            System.out.println("This is not a duplex printer");
+            return pages;
+        }
+    }
+}
