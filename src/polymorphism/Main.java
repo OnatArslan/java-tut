9e082abe642e76455d056649763992a3f3cf025d
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 01:36:26 +0300

    Learning encapsulation

---
+++ src/polymorphism/Main.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Movie movie = new Movie("Game of Thrones");
+        movie.watchMovie();
+    }
+}
