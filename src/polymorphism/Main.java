3d4b67f827d99ca01217ff6085799cd778fecc6b
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 01:43:44 +0300

    Commit for changes

--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -1,9 +1,19 @@
 package polymorphism;
 
+
+
+
 public class Main {
     public static void main(String[] args) {
 
         Movie movie = new Movie("Game of Thrones");
+        Movie adventureMovie = new Adventure("Game of Thrones");
         movie.watchMovie();
+        adventureMovie.watchMovie();
+
+
+
     }
 }
+
+
--- src/polymorphism/Movie.java
+++ src/polymorphism/Movie.java
@@ -13,3 +13,18 @@ public class Movie {
     }
 
 }
+
+class Adventure extends Movie{
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+}
