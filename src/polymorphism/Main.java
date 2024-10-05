af89c0b0705ca490fa79848db78865d3002d97e8
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 01:44:19 +0300

    Commit for changes

--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Movie movie = new Movie("Game of Thrones");
-        Movie adventureMovie = new Adventure("Game of Thrones");
+        Movie adventureMovie = new Adventure("Star Wars");
         movie.watchMovie();
         adventureMovie.watchMovie();
 
