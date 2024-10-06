d9cc42fdb2423d62b4bc21c7d42937a5ca9d3aac
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-06 03:39:03 +0300

    Commit for changes

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -1,6 +1,7 @@
 package arrays;
 
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
@@ -14,5 +15,19 @@ public class Main {
 
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(names));
+
+        int[] numbers2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] names2 = {"Onat", "Rugi", "Coni", "Charles"};
+
+        int numbers2Length = numbers2.length;
+        System.out.println("Length of number2 array is: "+ numbers2Length);
+
+        System.out.println("The last element of names2 array is: " + names2[names2.length -1]);
+
+        int accumulator = 0;
+        for (int i = 0; i < numbers2.length; i++) {
+            accumulator += numbers2[i];
+        }
+        System.out.println(accumulator);
     }
 }
