328851987e11b727082bf0b42724e8c5d1fdd206
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-07 04:38:25 +0300

    Commit for cahnges

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -31,9 +31,14 @@ public class Main {
         System.out.println(accumulator);
         /* Give values to array after initialization*/
         int[] newArray;
-        newArray = new int[]{5, 4, 3, 2, 1, 0};
+//        newArray = new int[]{5, 4, 3, 2, 1, 0};
+        newArray = new int[5];
         for (int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i]);
+            newArray[i] = newArray.length - i;
+        }
+
+        for(int element : newArray){
+            System.out.println(element);
         }
     }
 }
