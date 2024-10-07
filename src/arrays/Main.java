d692f002ebc69c5b205d045a858b3335c14fc4e6
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-07 04:21:24 +0300

    Commit for changes

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -30,6 +30,10 @@ public class Main {
         }
         System.out.println(accumulator);
         /* Give values to array after initialization*/
-
+        int[] newArray;
+        newArray = new int[]{5, 4, 3, 2, 1, 0};
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i]);
+        }
     }
 }
