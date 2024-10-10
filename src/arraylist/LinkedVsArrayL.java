43146bfc2b7a8f3dc07876f133f186e706d3bcdd
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-10 06:06:01 +0300

    Commit for changes

---
+++ src/arraylist/LinkedVsArrayL.java
@@ -0,0 +1,8 @@
+package arraylist;
+
+public class LinkedVsArrayL {
+    public static void main(String[] args) {
+        /* If we are getting or setting elements ArrayList is much more sense performance*/
+        /* If we are adding or removing elements LinkedList is much more sense and performance*/
+    }
+}
