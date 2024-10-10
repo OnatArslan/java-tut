6abfece2c2f7ea9bfb8d4745a9cf28678bc0b869
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-10 04:42:21 +0300

    Commit for changes

--- src/arraylist/ArrList.java
+++ src/arraylist/ArrList.java
@@ -5,15 +5,22 @@ import java.util.List;
 
 public class ArrList {
     public static void main(String[] args) {
+        /* Create regular array */
         String[] items = {"apples", "bananas", "milk", "eggs"};
 
+        /* Convert it to list type with List.of() function */
         List<String> list = List.of(items);
         System.out.println(list);
 
-        /* In paranthesis I give list to arraylist constructor and this will give me an arraylist with these items */
+        /* In parenthesis, I give a list to arraylist constructor and this will give me an arraylist with these items */
         ArrayList<String> groceries = new ArrayList<>(list);
         groceries.add("yogurt");
         System.out.println(groceries);
+
+        /* Shorthand version*/
+        ArrayList<String> myStringList = new ArrayList<>(List.of("Onat", "Rugi", "Coni", "Conitili"));
+        myStringList.addAll(0, groceries);
+        System.out.println(myStringList);
 
     }
 }
