63ab09e930d9fb36bbb189b68c723573ddf7ec35
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-15 00:47:49 +0300

    Commit for autoboxing

--- src/autoboxing/Main.java
+++ src/autoboxing/Main.java
@@ -3,8 +3,11 @@ package autoboxing;
 public class Main {
     public static void main(String[] args) {
 
-//        This is boxing
+//      This is written primitive with boxing
         Integer boxedInt = Integer.valueOf(15);
+
+//        Autoboxing
+        Integer boxedInt2 = 15;
 
 
     }
