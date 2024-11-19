505cb38e9b04b06eabff09172a2dc12032cf4cbd
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-11-19 19:54:23 +0300

    Commit for changes

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,7 @@ public class Main {
         System.out.println(myByte);
         System.out.println(myByte);
         System.out.println(myByte);
+        System.out.println(myByte);
     }
     public static boolean isEven(int number){
         return number % 2 == 0;
