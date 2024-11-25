eb2e347094fce6c180502e91823b47a499d4ab97
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-11-25 21:40:23 +0300

    Commit for changes

--- src/encapsulation/Main.java
+++ src/encapsulation/Main.java
@@ -25,5 +25,6 @@ public class Main {
         System.out.println(nonDublexPrinter.getPagesPrinted());
         System.out.println(nonDublexPrinter.getPagesPrinted());
         System.out.println(nonDublexPrinter.getPagesPrinted());
+        System.out.println(nonDublexPrinter.getPagesPrinted());
     }
 }
